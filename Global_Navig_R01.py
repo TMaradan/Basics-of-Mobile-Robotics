e699030b2ef469af547e10054b26c42ce1e4f963
TMaradan <119044814+TMaradan@users.noreply.github.com>
2022-12-04 10:17:12 +0100

    Add files via upload

--- Global_Navig_R01.py
+++ Global_Navig_R01.py
@@ -5,33 +5,35 @@ import matplotlib.pyplot as plt
 start = np.array([0.5,0.5])
 goal = np.array([5.25,6])
 
-polygon_1 = np.array([[1,1,5,5,2,2],[2,6,6,5,5,2]])
-polygon_2 = np.array([[1,1,3,3,4,4],[1,2,2,4.5,4.5,1]])
-
-# Find better way to add first point to each row of each polygon to close the obstacle
-
-#for i in range(len(polygon_1)):
-#    polygon_1[len(polygon_1[0])] = polygon_1[i][0]
-#    polygon_2[len(polygon_2[0])] = polygon_2[i][0]
-
-polygons_array = np.concatenate((polygon_1,polygon_2))
-
 start_point = vg.Point(start[0], start[1])
 goal_point = vg.Point(goal[0], goal[1])
 
+vertices = np.array([[[1,2],[3,2],[3,4],[1,4]],[[4,4],[6,4],[5,5]]])
+
+vertices = np.asarray(vertices)
+
+plt.plot(vertices)
+plt.show
+
 # Automate the creation of vg.Points for the obstacles
 
-#polygons = []
+max_vertices = 0
 
-#for i in range(int(len(polygons_array)/2)):
-#    for j in range(len(polygons_array[1])):
-#        polygons.append(vg.Point(polygons_array[2*i][j],polygons_array[2*i][j]))
+for i in range(len(vertices)):
+    if(len(vertices[i])>max_vertices):
+        max_vertices = len(vertices[i])
 
-polygons = [[vg.Point(1.0,2.5), vg.Point(1.0,6.0), vg.Point(5.0,6.0), vg.Point(5.0,5.0), vg.Point(2.0,5.0), vg.Point(2.0,2.5), vg.Point(1.0,2.5)],
-            [vg.Point(1.0,1.0), vg.Point(1.0,2.0), vg.Point(3.0,2.0), vg.Point(3.0,4.5), vg.Point(4.0,4.5), vg.Point(4.0,1.0), vg.Point(1.0,1.0)]]
+polygons = [[0 for i in range(max_vertices+1)] for j in range(len(vertices))]
+
+for i in range(len(vertices)):
+    for j in range(len(vertices[i])):
+        polygons[i][j] = vg.Point(vertices[i][j][0],vertices[i][j][1])
+    polygons[i][(len(vertices[i])):(max_vertices+1)] = [polygons[i][0] for k in range(len(vertices[i]),max_vertices+1)]
+    #polygons[i][range(len(vertices[i]),max_vertices)] = polygons[i][0]
 
 graph = vg.VisGraph()
 graph.build(polygons)
+#graph.buffer(margin,join_style=2)
 
 shortest_path = graph.shortest_path(start_point,goal_point)
 
@@ -46,7 +48,6 @@ for i in range(len(polygons)):
         polygons_y[i][j] = polygons[i][j].y
     polygons_x[i][len(polygons[1])] = polygons_x[i][0]
     polygons_y[i][len(polygons[1])] = polygons_y[i][0]
-
 
 shortest_path_x = np.empty(len(shortest_path))
 shortest_path_y = np.empty(len(shortest_path))
